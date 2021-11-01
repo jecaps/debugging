13740cd31a201f3c894e25410654aa4323513d7b
Jerika Kanz <jerikacapila@gmail.com>
2021-11-01 14:04:14 +0100

    Use the JavaScript Console to Check the Value of a Variable

---
+++ console.js
@@ -0,0 +1,14 @@
+/* The console.log() method, which "prints" the output of what's within its parentheses to the console, will likely be the most helpful debugging tool.
+  Placing it at strategic points in your code can show you the intermediate values of variables.*/
+
+console.log("Hello world!");
+
+// Challenge
+
+let a = 5;
+let b = 1;
+a++;
+console.log(a);
+
+let sumAB = a + b;
+console.log(sumAB);
