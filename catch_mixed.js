cd5d6feaf37f940b405c641b7c30f32c1357b243
Jerika Kanz <jerikacapila@gmail.com>
2021-11-01 16:07:33 +0100

    Catch Catch Mixed Usage of Single and Double Quotes

---
+++ catch_mixed.js
@@ -0,0 +1,16 @@
+/* JavaScript allows the use of both single (') and double (") quotes to declare a string. 
+  Deciding which one to use generally comes down to personal preference, with some exceptions.
+  Having two choices is great when a string has contractions or another piece of text that's in quotes. 
+  Just be careful that you don't close the string too early, which causes a syntax error.*/
+
+const grouchoContraction =
+  "I've had a perfectly wonderful evening, but this wasn't it.";
+const quoteInString =
+  "Groucho Marx once said 'Quote me as saying I was mis-quoted.'";
+const allSameQuotes =
+  "I've had a perfectly wonderful evening, but this wasn't it.";
+
+// Challenge
+
+let innerHtml = '<p>Click here to <a href="#Home">return home</a></p>';
+console.log(innerHtml);
